78a0f51a07af218140f1b05efe17c9e2fbdd8a24
Edvan Macedo <edvan.macedo.jr@gmail.com>
2016-12-26 11:31:15 -0400

    Implement class media that provides data base for movies

--- movie-trailler/media.py
+++ movie-trailler/media.py
@@ -7,15 +7,22 @@ Created on Mon Dec 26 10:32:52 2016
 
 import videoinfo
 
-class Media(videoinfo.VideoInfo):
-
+class Movie(videoinfo.VideoInfo):
+    
+    """ This class provides the data base for movie
+    Attributes:
+        duration (str): This should inform the media duration
+        ratings (str): This should inform the valid ratings of media
+        genre (str): This should inform the genre of media
+        date(str): This should inform the date of release of media
+        movie_title(str): This should inform the movie title
+        movie_storyline(str): This should inform short description of movie
+        poster_image(str): This should provide the URL image for movie poster
+        trailer_youtube(str): This should provide the URL for movie trailer
+    """
     def __init__(self, duration, ratings, genre, date, movie_title, movie_storyline, poster_image, trailer_youtube):
         videoinfo.VideoInfo.__init__(self, duration, ratings, genre, date)        
         self.movie_title = movie_title
         self.movie_storyline = movie_storyline
         self.poster_image = poster_image
         self.trailer_youtube = trailer_youtube
-
-moviemedia = Media("1 hora", videoinfo.VideoInfo.VALID_RATINGS[2], "comedy", "1998", "filme teste", "filme descricao", "url image", "url trailler")
-
-print(moviemedia.ratings)
